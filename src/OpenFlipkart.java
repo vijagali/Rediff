cf5b10d94f33a390f4d7ceed12dd91d86e21dcd4
vijagali <vikasjagali234453@gmail.com>
2020-02-24 23:43:05 +0530

    Flikart class added

---
+++ src/OpenFlipkart.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenFlipkart {
+	
+	public static void main(String args[]){
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\SeleniumBrowserDrivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+	}
+
+}
