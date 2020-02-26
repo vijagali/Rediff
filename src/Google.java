9553bb99bf2f2548e5dff0a12ccabf794f53e0d0
vijagali <vikasjagali234453@gmail.com>
2020-02-26 07:46:39 +0530

    google class modifies

--- src/Google.java
+++ src/Google.java
@@ -3,9 +3,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Google {
 	
+	public static WebDriver driver;
+	
 	public static void main(String args[]){
 		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\SeleniumBrowserDrivers\\chromedriver.exe");
-		WebDriver driver;
+		
 		driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
